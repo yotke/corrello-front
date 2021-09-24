363f53fb27248c643c06834b9802fdff4194423b
yotke <yotam.elia@gmail.com>
2021-09-24 17:48:54 +0300

    header and all workspace changes

--- src/pages/workspace.jsx
+++ src/pages/workspace.jsx
@@ -41,7 +41,7 @@ class _Workspace extends React.Component {
                             <h1>header of Workspace </h1>
                         </div>
                     </div>
-                    <ul><li class="tabbed-pane-nav-item"><a class="tabbed-pane-nav-item-button js-org-profile active" data-tab="boards" href={`/${user}`}>Boards</a></li></ul>
+                    <ul><li className="tabbed-pane-nav-item"><a className="tabbed-pane-nav-item-button js-org-profile active" data-tab="boards" href={`/${user}`}>Boards</a></li></ul>
                 </div>
                 <main>
                     <div className="nav-bar-workspace-container">
@@ -54,7 +54,7 @@ class _Workspace extends React.Component {
                                 <button className="btn-workspace-views" onClick={() => {
                                     this.setState({ isOpen: !isOpen })
                                 }}>
-                                    <span classNmae="sc-bdVaJa dWodlR" role="img" aria-label="UpIcon">
+                                    <span className="sc-bdVaJa dWodlR" role="img" aria-label="UpIcon">
                                         {isOpen && <svg width="15" height="15" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M12.7071 7.29289L19.7782 14.364C20.1687 14.7545 20.1687 15.3876 19.7782 15.7782C19.3877 16.1687 18.7545 16.1687 18.364 15.7782L12 9.41421L5.63606 15.7782C5.24554 16.1687 4.61237 16.1687 4.22185 15.7782C3.83132 15.3877 3.83132 14.7545 4.22185 14.364L11.2929 7.29289C11.6834 6.90237 12.3166 6.90237 12.7071 7.29289Z" fill="currentColor">
                                             </path>
