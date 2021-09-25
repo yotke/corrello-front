067d82da3e76b7cdfcef4436a117d50049651578
yotke <yotam.elia@gmail.com>
2021-09-25 09:38:25 +0300

    with get started in homepage

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -26,7 +26,19 @@ class _HomePage extends React.Component {
                         <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/spirit/hero/6a3ccd8e5c9a0e8ebea4235d12da6b24/hero.png" alt="" />
                     </div>
                 </div>
+                <div className="second-home-page-container">
+                    <h2>
+                        It’s more than work. It’s a way of working together.
+                    </h2>
+                    <p className="first-part-txt">
 
+                        Start with a Trello board, lists, and cards. Customize and expand with more features as your teamwork grows.
+                        Manage projects, organize tasks, and build team spirit—all in one place.
+                    </p>
+                    <button type="submit" data-analytics-button="greenSignupHeroButton" className="btn btn-home-page-submit " onClick={() => {
+                       this.props.history.push('/workspace')
+                    }}>Get Started</button>
+                </div>
             </section >
         )
     }
