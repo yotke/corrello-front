fe867f9bcf98d1a723da7707b8cfc0580d1d1b76
yotke <yotam.elia@gmail.com>
2021-09-30 10:08:04 +0300

    logo and all login/home page changes

--- src/cmps/login-signup.jsx
+++ src/cmps/login-signup.jsx
@@ -12,6 +12,7 @@ export class _LoginSignup extends React.Component {
     }
     componentDidMount() {
         const { isSignup } = this.props
+  
         this.setState({ isSignup })
     }
     clearState = () => {
