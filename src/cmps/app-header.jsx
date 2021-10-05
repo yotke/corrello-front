2b5f99ceac3fe2c505663f234e01557bb1c23154
yotke <yotam.elia@gmail.com>
2021-10-06 00:35:22 +0300

    after checklist update

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -12,11 +12,26 @@ import { LoginSignup } from './login-signup.jsx'
 
 class _AppHeader extends React.Component {
 
+
+
     state = {
         filterTxt: '',
         currOpenModal: '',
         isNewNotific: false,
     }
+
+componentDidMount(){
+    this.searchInput = React.createRef();
+}
+
+
+    handleChange = (ev) => {
+        const title = ev.target.name;
+        const value = ev.target.type
+    };
+
+
+
 
     onOpenPopover = (ev, PopoverName) => {
         //debugger
@@ -31,7 +46,6 @@ class _AppHeader extends React.Component {
     render() {
         const { isNewNotific } = this.state
         var { isBoardStyle, openPopover, user } = this.props
-
         const [name, lastname] = user ? user.fullname.split(' ') : ['', '']
         console.log(name[0])
         console.log(lastname[0])
@@ -43,7 +57,7 @@ class _AppHeader extends React.Component {
                 <div className={`main-header flex  ${!isBoardStyle ? 'in-board' : 'out-board'}`}>
 
                     <button className="btn-header flex" >
-                            <svg width="20" height="20" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M4 5C4 4.44772 4.44772 4 5 4H7C7.55228 4 8 4.44772 8 5V7C8 7.55228 7.55228 8 7 8H5C4.44772 8 4 7.55228 4 7V5ZM4 11C4 10.4477 4.44772 10 5 10H7C7.55228 10 8 10.4477 8 11V13C8 13.5523 7.55228 14 7 14H5C4.44772 14 4 13.5523 4 13V11ZM11 4C10.4477 4 10 4.44772 10 5V7C10 7.55228 10.4477 8 11 8H13C13.5523 8 14 7.55228 14 7V5C14 4.44772 13.5523 4 13 4H11ZM10 11C10 10.4477 10.4477 10 11 10H13C13.5523 10 14 10.4477 14 11V13C14 13.5523 13.5523 14 13 14H11C10.4477 14 10 13.5523 10 13V11ZM17 4C16.4477 4 16 4.44772 16 5V7C16 7.55228 16.4477 8 17 8H19C19.5523 8 20 7.55228 20 7V5C20 4.44772 19.5523 4 19 4H17ZM16 11C16 10.4477 16.4477 10 17 10H19C19.5523 10 20 10.4477 20 11V13C20 13.5523 19.5523 14 19 14H17C16.4477 14 16 13.5523 16 13V11ZM5 16C4.44772 16 4 16.4477 4 17V19C4 19.5523 4.44772 20 5 20H7C7.55228 20 8 19.5523 8 19V17C8 16.4477 7.55228 16 7 16H5ZM10 17C10 16.4477 10.4477 16 11 16H13C13.5523 16 14 16.4477 14 17V19C14 19.5523 13.5523 20 13 20H11C10.4477 20 10 19.5523 10 19V17ZM17 16C16.4477 16 16 16.4477 16 17V19C16 19.5523 16.4477 20 17 20H19C19.5523 20 20 19.5523 20 19V17C20 16.4477 19.5523 16 19 16H17Z" fill="currentColor"></path></svg>
+                        <svg width="20" height="20" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M4 5C4 4.44772 4.44772 4 5 4H7C7.55228 4 8 4.44772 8 5V7C8 7.55228 7.55228 8 7 8H5C4.44772 8 4 7.55228 4 7V5ZM4 11C4 10.4477 4.44772 10 5 10H7C7.55228 10 8 10.4477 8 11V13C8 13.5523 7.55228 14 7 14H5C4.44772 14 4 13.5523 4 13V11ZM11 4C10.4477 4 10 4.44772 10 5V7C10 7.55228 10.4477 8 11 8H13C13.5523 8 14 7.55228 14 7V5C14 4.44772 13.5523 4 13 4H11ZM10 11C10 10.4477 10.4477 10 11 10H13C13.5523 10 14 10.4477 14 11V13C14 13.5523 13.5523 14 13 14H11C10.4477 14 10 13.5523 10 13V11ZM17 4C16.4477 4 16 4.44772 16 5V7C16 7.55228 16.4477 8 17 8H19C19.5523 8 20 7.55228 20 7V5C20 4.44772 19.5523 4 19 4H17ZM16 11C16 10.4477 16.4477 10 17 10H19C19.5523 10 20 10.4477 20 11V13C20 13.5523 19.5523 14 19 14H17C16.4477 14 16 13.5523 16 13V11ZM5 16C4.44772 16 4 16.4477 4 17V19C4 19.5523 4.44772 20 5 20H7C7.55228 20 8 19.5523 8 19V17C8 16.4477 7.55228 16 7 16H5ZM10 17C10 16.4477 10.4477 16 11 16H13C13.5523 16 14 16.4477 14 17V19C14 19.5523 13.5523 20 13 20H11C10.4477 20 10 19.5523 10 19V17ZM17 16C16.4477 16 16 16.4477 16 17V19C16 19.5523 16.4477 20 17 20H19C19.5523 20 20 19.5523 20 19V17C20 16.4477 19.5523 16 19 16H17Z" fill="currentColor"></path></svg>
                     </button>
                     <div className="logo flex ">
                         <Link to="/">
@@ -58,7 +72,7 @@ class _AppHeader extends React.Component {
                                 {/* <BoardIcon /> */}
                                 <div className="txt-btn-wraper">
                                     Workspace
-                                        <svg width="16" height="16" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M11.2929 16.7071L4.22185 9.63606C3.83132 9.24554 3.83132 8.61237 4.22185 8.22185C4.61237 7.83133 5.24554 7.83133 5.63606 8.22185L12 14.5858L18.364 8.22185C18.7545 7.83132 19.3877 7.83132 19.7782 8.22185C20.1687 8.61237 20.1687 9.24554 19.7782 9.63606L12.7071 16.7071C12.3166 17.0977 11.6834 17.0977 11.2929 16.7071Z" fill="currentColor"></path></svg>
+                                    <svg width="16" height="16" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M11.2929 16.7071L4.22185 9.63606C3.83132 9.24554 3.83132 8.61237 4.22185 8.22185C4.61237 7.83133 5.24554 7.83133 5.63606 8.22185L12 14.5858L18.364 8.22185C18.7545 7.83132 19.3877 7.83132 19.7782 8.22185C20.1687 8.61237 20.1687 9.24554 19.7782 9.63606L12.7071 16.7071C12.3166 17.0977 11.6834 17.0977 11.2929 16.7071Z" fill="currentColor"></path></svg>
                                 </div>
                                 {/* <ElementOverlay /> */}
                             </button>
@@ -67,13 +81,13 @@ class _AppHeader extends React.Component {
                             {/* <BoardIcon /> */}
                             <div className="txt-btn-wraper">
                                 Recent
-                                    <svg width="16" height="16" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M11.2929 16.7071L4.22185 9.63606C3.83132 9.24554 3.83132 8.61237 4.22185 8.22185C4.61237 7.83133 5.24554 7.83133 5.63606 8.22185L12 14.5858L18.364 8.22185C18.7545 7.83132 19.3877 7.83132 19.7782 8.22185C20.1687 8.61237 20.1687 9.24554 19.7782 9.63606L12.7071 16.7071C12.3166 17.0977 11.6834 17.0977 11.2929 16.7071Z" fill="currentColor"></path></svg>
+                                <svg width="16" height="16" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M11.2929 16.7071L4.22185 9.63606C3.83132 9.24554 3.83132 8.61237 4.22185 8.22185C4.61237 7.83133 5.24554 7.83133 5.63606 8.22185L12 14.5858L18.364 8.22185C18.7545 7.83132 19.3877 7.83132 19.7782 8.22185C20.1687 8.61237 20.1687 9.24554 19.7782 9.63606L12.7071 16.7071C12.3166 17.0977 11.6834 17.0977 11.2929 16.7071Z" fill="currentColor"></path></svg>
                             </div>
                         </button>
                         <button className="btn-header flex" onClick={(ev) => this.onOpenPopover(ev, 'STARRED')}>
                             <div className="txt-btn-wraper">
                                 Starred
-                                    <svg width="16" height="16" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M11.2929 16.7071L4.22185 9.63606C3.83132 9.24554 3.83132 8.61237 4.22185 8.22185C4.61237 7.83133 5.24554 7.83133 5.63606 8.22185L12 14.5858L18.364 8.22185C18.7545 7.83132 19.3877 7.83132 19.7782 8.22185C20.1687 8.61237 20.1687 9.24554 19.7782 9.63606L12.7071 16.7071C12.3166 17.0977 11.6834 17.0977 11.2929 16.7071Z" fill="currentColor"></path></svg>
+                                <svg width="16" height="16" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M11.2929 16.7071L4.22185 9.63606C3.83132 9.24554 3.83132 8.61237 4.22185 8.22185C4.61237 7.83133 5.24554 7.83133 5.63606 8.22185L12 14.5858L18.364 8.22185C18.7545 7.83132 19.3877 7.83132 19.7782 8.22185C20.1687 8.61237 20.1687 9.24554 19.7782 9.63606L12.7071 16.7071C12.3166 17.0977 11.6834 17.0977 11.2929 16.7071Z" fill="currentColor"></path></svg>
                             </div>
                         </button>
                         <button className="btn-header create-btn flex" >
@@ -84,8 +98,11 @@ class _AppHeader extends React.Component {
                             </div>
                         </button>
                     </div>
-                    <div className="btn-header-container flex">
-                        <div className="search-input-container">
+
+                    <div className="btn-header-container flex" >
+                        <div className="search-input-container"  ref={this.searchInput} onClick={(ev) => {this.onOpenPopover(ev, 'SEARCH_HEADER')
+                            
+                        }}>
                             <span className="sc-bdVaJa kBFJig" role="img" aria-label="SearchIcon">
                                 <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5C18 12.2269 17.4164 13.8175 16.4356 15.0852L20.3769 19.0953C20.764 19.4892 20.7586 20.1223 20.3647 20.5095C19.9708 20.8966 19.3376 20.8911 18.9505 20.4972L15.0129 16.4909C13.7572 17.4383 12.1942 18 10.5 18ZM16 10.5C16 13.5376 13.5376 16 10.5 16C7.46243 16 5 13.5376 5 10.5C5 7.46243 7.46243 5 10.5 5C13.5376 5 16 7.46243 16 10.5Z" fill="currentColor">
@@ -93,7 +110,7 @@ class _AppHeader extends React.Component {
                                 </svg>
                             </span>
                             <input autocomplete="off" autocorrect="off" spellcheck="false" className="search-input-header"
-                                data-test-id="header-search-input" id="9lkgOKT6X4ViGFKUiWS6OBVxAsVyJLk2" type="search" placeholder="Search" value="" />
+                                data-test-id="header-search-input" id="9lkgOKT6X4ViGFKUiWS6OBVxAsVyJLk2" type="search" placeholder="Search" value="" onChange={this.hndleChange} />
                         </div>
                         {/* <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5C18 12.2269 17.4164 13.8175 16.4356 15.0852L20.3769 19.0953C20.764 19.4892 20.7586 20.1223 20.3647 20.5095C19.9708 20.8966 19.3376 20.8911 18.9505 20.4972L15.0129 16.4909C13.7572 17.4383 12.1942 18 10.5 18ZM16 10.5C16 13.5376 13.5376 16 10.5 16C7.46243 16 5 13.5376 5 10.5C5 7.46243 7.46243 5 10.5 5C13.5376 5 16 7.46243 16 10.5Z" fill="currentColor"></path></svg> */}
 
---
+++ src/cmps/popover/popover-search-appheader.jsx
@@ -0,0 +1,57 @@
+
+import { connect } from 'react-redux';
+import { utilService } from '../../services/util.service';
+import { Component } from 'react';
+import { boardService } from '../../services/board.service';
+import { closePopover } from '../../store/popover.actions';
+import { onSaveBoard, loadRecentBoards } from "../../store/board.actions";
+import { Popover } from './Popover';
+import { Link, NavLink } from 'react-router-dom'
+class _PopoverSearchAppheader extends Component {
+    componentDidMount() {
+        this.props.loadRecentBoards()
+    }
+    render() {
+        const { boards } = this.props
+        const { recentBoards } = this.props
+        console.log('recentBoards in popover', recentBoards);
+        return <Popover>
+            <div className="recent-pop-over-content">
+                <ul className="board-list-navbar">
+                    {recentBoards && recentBoards.length > 0 && recentBoards.map((board) => {
+                        return (
+                            board && <Link to={`/board/${board._id}`} key={board._id} className="clean-link">
+                                <div className="nav-bar-btns flex row">
+                                    <li className="board-preview-navbar" key={board._id} style={board.style && {
+                                        backgroundImage: "url(" + board.style.background + ")",
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundAttachment: 'fixed',
+                                        backgroundPosition: 'center'
+                                    }}>
+                                    </li>
+                                    <h4 className="nav-bar-btns-name">{board.title}</h4>
+                                </div>
+                            </Link>
+                        );
+                    })}
+                </ul>
+            </div>
+        </Popover>
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        recentBoards: state.boardModule.recentBoards,
+        boards: state.boardModule.boards,
+    }
+}
+
+const mapDispatchToProps = {
+    closePopover,
+    loadRecentBoards
+}
+
+
+export const PopoverSearchAppheader = connect(mapStateToProps, mapDispatchToProps)(_PopoverSearchAppheader)
+
